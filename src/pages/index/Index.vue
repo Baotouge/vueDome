413e97e7e46fd7c85fb174cd5ad92c7824f0fc19
chenlei <cl123456>
2018-10-16 15:56:59 +0800

    提交文件

---
+++ src/pages/index/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='headr'>
+        hello index
+        <a href="login.html">login</a>
+        <hello></hello>
+    </div>
+
+</template>
+
+<script>
+import hello from '../../components/Hello'
+    export default {
+        name: '',
+        data() {
+            return {
+            };
+        },
+        components:{hello},
+        methods: {}
+    };
+</script>
+
+<style lang='less' scoped>
+
+</style>
